138fef992fa19e0f2e5f037270a00149d43e59ce
MichaelMulieri <mikemulieri@gmail.com>
2023-06-02 18:12:57 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -8,15 +8,21 @@ import {
 
 import Users from "./user/components/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" exact="true" element={<Users />} />
-        <Route path="/places/new" exact="true" element={<NewPlace />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" exact="true" element={<Users />} />
+          <Route path="/places/new" exact="true" element={<NewPlace />} />
+          <Route path="/:userId/places" exact="true" element={<UserPlaces />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
     </Router>
   );
 };
---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Params, useParams } from "react-router-dom";
+import PlaceList from "./components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2013/07/18/15/06/empire-state-building-164517_1280.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2020/02/29/23/03/new-york-4891327_1280.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u2",
+  },
+];
+
+const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;
